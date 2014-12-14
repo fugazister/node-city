672d79c9a7a79e1c25eaa7d5ff2978cb7a3a6763
Mikhail Khokhlov <hazolsky@gmail.com>
2014-12-14 12:23:15 +0600

    draw floor

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -41,11 +41,12 @@ var init = function () {
   document.body.appendChild(container);
 
   camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 15000);
-  camera.position.z = 50;
+  camera.position.z = 100;
+  camera.position.y = 20;
 
   controls = new FlyControls(camera);
   controls.movementSpeed = 10;
-  controls.rollSpeed = Math.PI/10;
+  controls.rollSpeed = Math.PI / 5;
 
   scene = new THREE.Scene();
   scene.fog = new THREE.Fog(0x000000, 1, 15000);
@@ -54,6 +55,40 @@ var init = function () {
   renderer.setSize( window.innerWidth, window.innerHeight );
   container.appendChild(renderer.domElement);
 
+  // GROUND
+  var groundCanvas = document.createElement( 'canvas' );
+  groundCanvas.width = 32;
+  groundCanvas.height = 32;
+
+  var groundContext = groundCanvas.getContext( '2d' );
+
+  groundContext.moveTo(0, groundCanvas.width / 2);
+  groundContext.lineTo(groundCanvas.width, groundCanvas.width / 2);
+  groundContext.moveTo(groundCanvas.width / 2, 0);
+  groundContext.lineTo(groundCanvas.width / 2, groundCanvas.width);
+  groundContext.strokeStyle = 'blue';
+  groundContext.stroke();
+
+  var groundTexture = new THREE.Texture( groundCanvas, THREE.UVMapping, THREE.RepeatWrapping, THREE.RepeatWrapping ),
+      groundMaterial = new THREE.MeshBasicMaterial( { map: groundTexture } );
+
+  groundTexture.needsUpdate = true;
+  groundTexture.repeat.set( 1000, 1000 );
+
+  var geometry = new THREE.PlaneBufferGeometry( 100, 100 );
+
+  var meshCanvas = new THREE.Mesh( geometry, groundMaterial );
+  meshCanvas.rotation.x = - Math.PI / 2;
+  meshCanvas.position.x = 0;
+  meshCanvas.position.y = -5;
+  meshCanvas.position.z = 0;
+  meshCanvas.scale.set( 100, 100, 100 );
+
+  scene.add( meshCanvas );
+
+
+
+  // CUBE
   var canvas = document.createElement( 'canvas' );
   canvas.width = 128;
   canvas.height = 128;
@@ -62,20 +97,11 @@ var init = function () {
   var gradient = context.createRadialGradient( canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2 );
   gradient.addColorStop( 0.1, 'rgba(0,0,150,1)' );
   gradient.addColorStop( 1, 'rgba(255,255,255,1)' );
-
   context.fillStyle = gradient;
   context.fillRect( 0, 0, canvas.width, canvas.height );
 
   var shadowTexture = new THREE.Texture( canvas );
   shadowTexture.needsUpdate = true;
-
-  var groundMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff, specular: 0x111111 } );
-
-  var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 20000, 20000 ), groundMaterial );
-  mesh.position.y = -250;
-  mesh.rotation.x = - Math.PI / 2;
-  mesh.receiveShadow = true;
-  scene.add( mesh );
 
   var cube = makeBox.call(scene, {
     width: 10,
