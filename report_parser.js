f434cd0ad7bfd96e322f289ec0f0d5cb151ecae4
Andrey Krivko <jastkand@gmail.com>
2014-12-14 13:02:47 +0600

    add initial implementation of object rendering with color

--- report_parser.js
+++ report_parser.js
@@ -3,7 +3,50 @@ var reportParser = function (report) {
       gutter = 1,
       parsed = report.reports[0].functions;
 
+
+  function Interpolate(start, end, steps, count) {
+    var s = start,
+        e = end,
+        final = s + (((e - s) / steps) * count);
+    return Math.floor(final);
+  }
+
+  function Color(_r, _g, _b) {
+      var r, g, b;
+      var setColors = function(_r, _g, _b) {
+          r = _r;
+          g = _g;
+          b = _b;
+      };
+
+      setColors(_r, _g, _b);
+      this.getColors = function() {
+          var colors = {
+              r: r,
+              g: g,
+              b: b
+          };
+          return colors;
+      };
+  }
+
+  var startColors = new Color(232, 9, 26).getColors();
+  var endColors = new Color(6, 170, 60).getColors();
+
   var result = parsed.map(function (f) {
+    var colorValue = f.cyclomaticDensity;
+    console.log(colorValue);
+
+    var r = Interpolate(startColors.r, endColors.r, 100, colorValue);
+    var g = Interpolate(startColors.g, endColors.g, 100, colorValue);
+    var b = Interpolate(startColors.b, endColors.b, 100, colorValue);
+
+    if (colorValue != null && colorValue != Infinity) {
+      var color = "rgb(" + r + "," + g + "," + b + ")";
+    } else {
+      var color = "#fff";
+    }
+
     var res = {
       x: index,
       y: 0,
@@ -11,7 +54,7 @@ var reportParser = function (report) {
       height: 1,
       width: 1,
       depth: 1,
-      color: '#fff'
+      color: color
     }
     index = index + gutter + 1;
     return res;
